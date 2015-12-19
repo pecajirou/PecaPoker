4139ab80b77cfc43de61f027cc98676e10983791
pecajirou <mat@iijmio-mail.jp>
2015-12-19 23:36:16 +0900

    キッカーの判定

--- src/com/pecapoker/texasholdem/ykPair.java
+++ src/com/pecapoker/texasholdem/ykPair.java
@@ -1,31 +1,39 @@
 package com.pecapoker.texasholdem;
 
-public class ykPair extends Yaku {
+import com.pecapoker.playingcards.CardSet;
+
+public class YkPair extends Yaku {
 	int pairNo;
 
 	public int getPairNo()
 	{
 		return pairNo;
 	}
+
 	@Override
 	protected int getYakuValue()
 	{
 		return 1;
 	}
-	public ykPair(FiveCard fiveCard, int pn) {
+	public YkPair(FiveCard fv, int pn) {
+		super(fv);
 		pairNo = pn;
 	}
 
-	//TODO テスト
 	@Override
 	public int compareTo(Yaku other) {
-		if ((this.getYakuValue() - other.getYakuValue() == 0)
-				&& (this.getPairNo() - ((ykPair)other).getPairNo() != 0)) {
-			return this.getPairNo() - ((ykPair)other).getPairNo();
+		// 役で差がつく
+		if (this.getYakuValue() - other.getYakuValue() != 0) {
+			return this.getYakuValue() - other.getYakuValue();
 		}
-		else {
-			return super.compareTo(other);
+		// ペアの数字で差がつく
+		if (this.getPairNo() - ((YkPair)other).getPairNo() != 0){
+			return this.getPairNo() - ((YkPair)other).getPairNo();
 		}
+		// キッカーで差がつく
+		CardSet kicker1 = this.getFiveCard().diffCardSetExceptRank(this.getPairNo());
+		CardSet kicker2 = other.getFiveCard().diffCardSetExceptRank(((YkPair)other).getPairNo());
+		return kicker1.compareTo(kicker2);
 	}
 
 }
